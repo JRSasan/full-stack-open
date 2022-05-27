e4974f9fa60b4f630b447fed99a175bb22dfb551
Jean Rae Sasan <jeanraesasan31@gmail.com>
2022-05-27 13:19:42 +0800

    submission for exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,7 +6,8 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
-const History = ({ feedback, text }) => {
+const Statistics = ({ feedback, text }) => {
+  console.log(feedback, text)
   return (
     <div>
       {text} {feedback}
@@ -14,22 +15,49 @@ const History = ({ feedback, text }) => {
   )
 }
 
+
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allfeedback, setAll] = useState(0)
 
   const handleGoodClick = () => {
     setGood(good+1)
+    setAll(allfeedback+1)
   }
   const handleNeutralClick = () => {
     setNeutral(neutral+1)
+    setAll(allfeedback+1)
   }
   const handleBadClick = () => {
     setBad(bad+1)
+    setAll(allfeedback+1)
   }
 
-  
+  const averageFeedback = () => {
+    let average
+    if (bad !== 0) {
+      average = (good - bad) / allfeedback
+    } else if (neutral !==0) {
+      average = good / allfeedback
+    } else {
+      average = good
+    }
+
+    return average
+  }
+
+  const positivePercent = () => {
+    let percentage = 0 
+    if (good !==0) {
+      percentage = (good/allfeedback) * 100
+    }
+    
+    return percentage + ' %'
+  }
+
   return (
     <div>
     <h1>give feedback</h1>
@@ -37,9 +65,12 @@ const App = () => {
     <Button handleClick={handleNeutralClick} text='neutral' />
     <Button handleClick={handleBadClick} text='bad' />
     <h1>statistics</h1>
-    <History feedback={good} text='good' />
-    <History feedback={neutral} text='neutral' />
-    <History feedback={bad} text='bad' />
+    <Statistics feedback={good} text='good' />
+    <Statistics feedback={neutral} text='neutral' />
+    <Statistics feedback={bad} text='bad' />
+    <Statistics feedback={allfeedback} text='all' />
+    <Statistics feedback={averageFeedback()} text='average' />
+    <Statistics feedback={positivePercent()} text='positive' />
     </div>
   )
 }
