57deabf872718ba366f3c3dfe8eca15c380f5fe0
Jean Rae Sasan <jeanraesasan31@gmail.com>
2022-06-01 18:33:36 +0800

    submission of exercises 2.1-2.5

---
+++ part2/courseinfo/src/Course.js
@@ -0,0 +1,27 @@
+const Header = ({ course }) => <h2>{course}</h2>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+  return (
+  <div>
+    {parts.map(part => <Part key={part.id} part={part} />)}
+    <p>
+      <strong>total of {total} exercises</strong>
+    </p>
+  </div>
+  )
+} 
+
+const Course = ({course}) => (
+  <div>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+  </div>
+)
+
+export default Course
