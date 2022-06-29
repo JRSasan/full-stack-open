b02b5aa66f8de9020a188f5be36b2024f9f60ec1
Jean Rae Sasan <sasan.jeanrae@gmail.com>
2022-06-29 16:19:41 +0800

    Update index.js

--- part4/index.js
+++ part4/index.js
@@ -13,7 +13,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = 'mongodb+srv://jeanrae:polipo098@cluster0.qazfi.mongodb.net/blog-list?retryWrites=true&w=majority'
 mongoose.connect(mongoUrl)
 
 app.use(cors())
